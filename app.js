09f248a0161b9879846213c580c2b2de349db1d0
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-07-20 18:00:14 +0530

    Locomotive js topic learn completed

---
+++ app.js
@@ -0,0 +1,7 @@
+// Step 1: Go to the google and search "locomotive js github" 
+//     Step 2: Click the first link
+//         Step 3: After that scroll down and find "Smooth" and copy second box 👇
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
